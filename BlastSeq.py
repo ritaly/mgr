bc461bb054027eae5ebef18ea6740203b91c1fb7
Malgorzata Rita <malgorzata.lyczywek@gmail.com>
2017-05-01 20:52:08 +0200

    delete txt

--- BlastSeq.py
+++ BlastSeq.py
@@ -1,14 +1,18 @@
 # -*- coding: cp1250 -*-
-
 """ wczytuje plik 1
-porownuje z wszystkimi plikami odległymi > min && < max
-wykonuje porównanie parami
+porownuje z wszystkimi plikami odlegĹ‚ymi > min && < max
+wykonuje porĂłwnanie parami
 zapis do pliku: A ;B;C;D gdzie:
 A - pozycje pliku 1
 B - pozycje pliku 2
-C - procen podobieństwa
-D - procen pokrycia
+C - procent podobieĹ„stwa
+D - procent pokrycia """
+
+import os
+
+path = './wynik/'
+
+for filename in os.listdir(path):
+    print filename
 
     
-    
-
