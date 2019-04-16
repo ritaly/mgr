caacbecd4d07702774a2d096776a4196cae4485e
ritaly <malgorzata.lyczywek@gmail.com>
2019-04-16 02:53:17 +0200

    results

--- AnalyseBlastResult.py
+++ AnalyseBlastResult.py
@@ -3,11 +3,6 @@ import time
 start_time = time.time()
 print(time.strftime("%d-%b-%Y-%H:%M:%S", time.localtime()))
 
-def check_start(current, last):
-	query_cond = current[4] >= last[0] and current[4] < last[1]
-	subject_cond = current[6] >= last[2] and current[6] < last[3]
-	return (query_cond and subject_cond)
-
 def convert(value):
 	if value == '':
 		return ''
@@ -15,6 +10,11 @@ def convert(value):
 		 return float(value)
 	else:
 		return int(value)
+
+def check_start(current, last):
+	query_cond = current[4] >= last[0] and current[4] < last[1]
+	subject_cond = current[6] >= last[2] and current[6] < last[3]
+	return (query_cond and subject_cond)
 
 def check_qend(current, last):
 	return current[5] >= last[1]
@@ -27,7 +27,7 @@ def check_send(current, last):
 
 array_with_LCR = [] #ad 1 [qstar, qend,sstart,send]
 
-with open('25_lines.txt', 'r') as file:
+with open('chr_test_2019_15-Apr-2019-200538.txt', 'r') as file:
 
 	data = file.readlines()
 	#import code; code.interact(local=dict(globals(), **locals()))
@@ -38,18 +38,21 @@ with open('25_lines.txt', 'r') as file:
 		prev = [convert(i) for i in prev_line.split()] 
 		if prev_line == '':
 			prev_line = curr_line
-			array_with_LCR.append([curr[4], curr[5], curr[6], curr[7]])
+			array_with_LCR.append([curr[4], curr[5], curr[6], curr[7], curr[2]])
 			continue
 
 		dist = curr[0] - prev[0]
-		if dist > 500:
+		dist2 = curr[1] - prev[1]
+		if dist > 500 or dist2 > 500:
 			#print(int(prev[0]), int(curr[0]))
-			array_with_LCR.append([curr[4], curr[5], curr[6], curr[7]])
+			array_with_LCR.append([curr[4], curr[5], curr[6], curr[7], curr[2]])
 			prev_line = curr_line
 
-		if dist == 0 or dist == 500:
+		if (dist == 0 or dist == 500) and (dist2 == 0 or dist2 == 500):
 			last_align = array_with_LCR[-1]
 			if check_start(curr, last_align):
+				if curr[2] > last_align[4]:
+					last_align[4] = curr[2]
 				#to jest jaknajbardziej ok
 				if check_qend(curr, last_align):
 					last_align[1] = curr[5]
@@ -62,7 +65,7 @@ with open('25_lines.txt', 'r') as file:
 			#czy nowe qstart >= prev_qstart ale =< prev_end
 
 timestamp = time.strftime("%d-%b-%Y-%H%M%S", time.localtime())
-save_file = open("chr3_result_2019_" + timestamp + ".txt", "w")
+save_file = open("chr3_result_2019_" + timestamp + "NOC.txt", "w")
 
 for i in array_with_LCR:
 	align_len = i[1] - i[0]
